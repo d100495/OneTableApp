cd3aa13d00110b679d16a8c4748cb13af25b9bb0
Zscfg <ranoco2@gmail.com>
2018-05-15 15:41:56 +0200

    Addded worker-form.component

--- Fronted/src/app/app.module.ts
+++ Fronted/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,18 +7,21 @@ import { AppComponent } from './app.component';
 import { WorkersFromMsSqlDatabaseService } from '../Services/WorkersFromMsSqlDatabaseService';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { WorkerFormComponent } from './worker-form/worker-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkerFormComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [WorkersFromMsSqlDatabaseService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, WorkerFormComponent]
 })
 export class AppModule { }
